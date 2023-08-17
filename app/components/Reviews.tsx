2aebc275e4919b3bd348fd1317ad43a3b616298d
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-18 01:07:06 +1000

    added pagination and wishlist

--- app/components/Reviews.tsx
+++ app/components/Reviews.tsx
@@ -1,6 +1,8 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic"
+
 async function Reviews({ id }: { id: string }) {
   const supabase = createServerComponentClient({ cookies });
   let response = await supabase.from("reviews").select().eq("product_id", id);
