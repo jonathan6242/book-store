662e6ad1458e1a89770542b6a9bf8f108cfb9d19
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-19 02:01:54 +1000

    attempt to fix mobile bug

--- app/components/BookDescription.tsx
+++ app/components/BookDescription.tsx
@@ -15,25 +15,30 @@ function BookDescription({
 }) {
   const [showMore, setShowMore] = useState(false);
   const descriptionRef = useRef<HTMLParagraphElement>(null);
+  const shadowRef = useRef<HTMLParagraphElement>(null);
   const [scrollHeight, setScrollHeight] = useState<number | undefined>(
     descriptionRef?.current?.scrollHeight
   );
   const [offsetHeight, setOffsetHeight] = useState<number | undefined>(
     descriptionRef?.current?.offsetHeight
   );
+  const [shadowHeight, setShadowHeight] = useState<number | undefined>(
+    shadowRef?.current?.offsetHeight
+  );
 
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   const onResize = () => {
-    if (
-      !isMobile &&
-      (descriptionRef?.current?.scrollHeight! <=
-      descriptionRef?.current?.offsetHeight!)
-    ) {
-      setShowMore(false);
-    }
+    // if (
+    //   !isMobile &&
+    //   descriptionRef?.current?.scrollHeight! <=
+    //     descriptionRef?.current?.offsetHeight!
+    // ) {
+    //   setShowMore(false);
+    // }
     setScrollHeight(descriptionRef?.current?.scrollHeight);
     setOffsetHeight(descriptionRef?.current?.offsetHeight);
+    setShadowHeight(shadowRef?.current?.offsetHeight);
   };
 
   useLayoutEffect(() => {
@@ -46,7 +51,15 @@ function BookDescription({
 
   return (
     <>
-      <div className={`leading-loose mb-6`}>
+      <div className={`leading-loose mb-6 relative`}>
+        <p
+          className={`absolute opacity-0 inset-x-0 top-0 ${
+            selected ? "line-clamp-6" : "line-clamp-4"
+          }`}
+          ref={shadowRef}
+        >
+          {description}
+        </p>
         <p
           className={`${
             !showMore ? `${selected ? "line-clamp-6" : "line-clamp-4"}` : ""
@@ -55,7 +68,8 @@ function BookDescription({
         >
           {description}
         </p>
-        {scrollHeight! > offsetHeight! && (
+
+        {scrollHeight! > shadowHeight! && (
           <button
             onClick={() => setShowMore(!showMore)}
             className={`text-lime-500 underline underline-offset-4 font-medium`}
