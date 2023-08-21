5db309f6289f7fcd6719189e3ddfeab9e94288a6
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-22 02:45:13 +1000

    re-added reviews loader to improve loading times

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -5,19 +5,11 @@ import BookPageSkeleton from "@/app/components/(skeletons)/BookPageSkeleton";
 import RecommendedBooks from "@/app/components/RecommendedBooks";
 import RecommendedBooksSkeleton from "@/app/components/(skeletons)/RecommendedBooksSkeleton";
 import { Suspense } from "react";
-import Reviews from "@/app/components/Reviews";
 import ReviewForm from "@/app/components/ReviewForm";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers"
 import ReviewsLoader from "@/app/components/ReviewsLoader";
 
 async function BookPage({ params }: { params: { id: string } }) {
   const { id } = params;
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({
-    cookies: () => cookieStore
-  })
-  const { data, count }: { data: any[] | null, count: any } = await supabase.from("reviews").select("*", { count: "exact" }).eq("product_id", id).order('date', { ascending: false });
 
   return (
     <div className="row wrapper">
