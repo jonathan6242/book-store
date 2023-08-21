6f7cdbdc965deef4ab97b5bf116d682f858483c9
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-22 03:04:34 +1000

    added cooldown to wishlist toggle

--- app/components/BookInfoImage.tsx
+++ app/components/BookInfoImage.tsx
@@ -1,39 +1,52 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
-import { BookProduct } from "../(utils)/types"
-import Image from "next/image"
+import { BookProduct } from "../(utils)/types";
+import Image from "next/image";
 import { toast } from "react-hot-toast";
 
-function BookInfoImage({ book, selected } : { book: BookProduct, selected?: boolean }) {
+function BookInfoImage({
+  book,
+  selected,
+}: {
+  book: BookProduct;
+  selected?: boolean;
+}) {
   const originalPrice = book.metadata?.originalPrice;
   const images = book?.images || [];
   const { name, id: productId } = book;
   const [inWishlist, setInWishlist] = useState(false);
+  const [cooldown, setCooldown] = useState(false);
 
-  const toggleWishlist = (e: any) => {
+  const toggleWishlist = async (e: any) => {
     e.preventDefault();
-    let wishlist;
-    let message: string = '';
-    if (localStorage.getItem("wishlist")) {
-      wishlist = JSON.parse(localStorage.getItem("wishlist")!);
-      if(wishlist.includes(productId)) {
-        wishlist = wishlist.filter((elem: string) => elem !== productId);
-        setInWishlist(false);
-        message = `${name} removed from wishlist`
+    if (!cooldown) {
+      setCooldown(true);
+      let wishlist;
+      let message: string = "";
+      if (localStorage.getItem("wishlist")) {
+        wishlist = JSON.parse(localStorage.getItem("wishlist")!);
+        if (wishlist.includes(productId)) {
+          wishlist = wishlist.filter((elem: string) => elem !== productId);
+          setInWishlist(false);
+          message = `${name} removed from wishlist`;
+        } else {
+          wishlist.push(productId);
+          setInWishlist(true);
+          message = `${name} added to wishlist`;
+        }
       } else {
-        wishlist.push(productId);
+        wishlist = [productId];
         setInWishlist(true);
-        message = `${name} added to wishlist`
+        message = `${name} added to wishlist`;
       }
-    } else {
-      wishlist = [productId];
-      setInWishlist(true);
-      message = `${name} added to wishlist`
+      localStorage.setItem("wishlist", JSON.stringify(wishlist));
+      toast.success(message, {
+        position: "bottom-center",
+      });
+      setTimeout(() => {
+        setCooldown(false);
+      }, 2000);
     }
-    localStorage.setItem("wishlist", JSON.stringify(wishlist));
-    toast.success(message, {
-      position: "bottom-center",
-    });
   };
 
   useEffect(() => {
@@ -45,11 +58,12 @@ function BookInfoImage({ book, selected } : { book: BookProduct, selected?: bool
     }
   }, []);
 
-
   return (
     <figure
-      className={`relative w-full ${!selected ? 'max-w-sm md:max-w-xs' : 'xs:max-w-xs'}  lg:max-w-sm shadow-lg flex-shrink-0`}
-      data-aos={`${!selected ? 'fade-right' : ''}`}
+      className={`relative w-full ${
+        !selected ? "max-w-sm md:max-w-xs" : "xs:max-w-xs"
+      }  lg:max-w-sm shadow-lg flex-shrink-0`}
+      data-aos={`${!selected ? "fade-right" : ""}`}
       data-aos-delay="200"
       data-aos-duration="800"
       data-aos-once={true}
@@ -64,16 +78,16 @@ function BookInfoImage({ book, selected } : { book: BookProduct, selected?: bool
       />
       {originalPrice && <div className="bookmark-lg">Sale!</div>}
       <button
-          onClick={toggleWishlist}
-          className="absolute top-4 left-4 w-12 h-12 bg-white rounded-full grid place-items-center shadow text-center"
-        >
-          <i
-            className={`${
-              inWishlist ? "fa-solid" : "fa-regular"
-            } fa-heart leading-9 text-2xl`}
-          ></i>
-        </button>
+        onClick={toggleWishlist}
+        className="absolute top-4 left-4 w-12 h-12 bg-white rounded-full grid place-items-center shadow text-center"
+      >
+        <i
+          className={`${
+            inWishlist ? "fa-solid" : "fa-regular"
+          } fa-heart leading-9 text-2xl`}
+        ></i>
+      </button>
     </figure>
-  )
+  );
 }
-export default BookInfoImage
+export default BookInfoImage;
